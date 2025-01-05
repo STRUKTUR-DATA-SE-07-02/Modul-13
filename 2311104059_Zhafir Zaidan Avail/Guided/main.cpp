127dc77a19f232d8219e6af5f49352a24dba5cca
Zhafir Avel <167537812+zhafiravel@users.noreply.github.com>
2025-01-05 16:20:52 +0700

    Add files via upload

---
+++ 2311104059_Zhafir Zaidan Avail/Guided/main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+struct EmployeeNode {
+    string name;
+    EmployeeNode* next;
+    EmployeeNode* subordinate;
+
+    EmployeeNode(string empName) : name(empName), next(nullptr), subordinate(nullptr) {}
+};
+
+class EmployeeList {
+private:
+    EmployeeNode* head;
+
+public:
+    EmployeeList() : head(nullptr) {}
+
+    void addEmployee(string name) {
+        EmployeeNode* newEmployee = new EmployeeNode(name);
+        newEmployee->next = head;
+        head = newEmployee;
+    }
+
+    void addSubordinate(string managerName, string subordinateName) {
+        EmployeeNode* manager = head;
+        while (manager != nullptr && manager->name != managerName) {
+            manager = manager->next;
+        }
+        if (manager != nullptr) {
+            EmployeeNode* newSubordinate = new EmployeeNode(subordinateName);
+            newSubordinate->next = manager->subordinate;
+            manager->subordinate = newSubordinate;
+        } else {
+            cout << "Manager not found!" << endl;
+        }
+    }
+
+    void deleteEmployee(string name) {
+        EmployeeNode** current = &head;
+        while (*current != nullptr && (*current)->name != name) {
+            current = &((*current)->next);
+        }
+
+        if (*current != nullptr) {
+            EmployeeNode* toDelete = *current;
+            *current = (*current)->next;
+
+            while (toDelete->subordinate != nullptr) {
+                EmployeeNode* subTemp = toDelete->subordinate;
+                toDelete->subordinate = toDelete->subordinate->next;
+                delete subTemp;
+            }
+            delete toDelete;
+            cout << "Employee " << name << " deleted." << endl;
+        } else {
+            cout << "Employee not found!" << endl;
+        }
+    }
+
+    void deleteSubordinate(string managerName, string subordinateName) {
+        EmployeeNode* manager = head;
+        while (manager != nullptr && manager->name != managerName) {
+            manager = manager->next;
+        }
+
+        if (manager != nullptr) {
+            EmployeeNode** currentSub = &(manager->subordinate);
+            while (*currentSub != nullptr && (*currentSub)->name != subordinateName) {
+                currentSub = &((*currentSub)->next);
+            }
+
+            if (*currentSub != nullptr) {
+                EmployeeNode* toDelete = *currentSub;
+                *currentSub = (*currentSub)->next;
+
+                delete toDelete;
+                cout << "Subordinate " << subordinateName << " deleted from " << managerName << "." << endl;
+            } else {
+                cout << "Subordinate not found!" << endl;
+            }
+        } else {
+            cout << "Manager not found!" << endl;
+        }
+    }
+
+    void display() {
+        EmployeeNode* current = head;
+        while (current != nullptr) {
+            cout << "Manager: " << current->name << " -> ";
+            EmployeeNode* sub = current->subordinate;
+            while (sub != nullptr) {
+                cout << sub->name << " ";
+                sub = sub->next;
+            }
+            cout << endl;
+            current = current->next;
+        }
+    }
+
+    ~EmployeeList() {
+        while (head != nullptr) {
+            EmployeeNode* temp = head;
+            head = head->next;
+
+            while (temp->subordinate != nullptr) {
+                EmployeeNode* subTemp = temp->subordinate;
+                temp->subordinate = temp->subordinate->next;
+                delete subTemp;
+            }
+            delete temp;
+        }
+    }
+};
+
+//int main() {
+//    EmployeeList empList;
+//
+//    empList.addEmployee("Alice");
+//    empList.addEmployee("Bob");
+//    empList.addEmployee("Charlie");
+//
+//    empList.addSubordinate("Alice", "David");
+//    empList.addSubordinate("Alice", "Eve");
+//    empList.addSubordinate("Bob", "Frank");
+//
+//    cout << "Initial employee list:" << endl;
+//    empList.display();
+//
+//    empList.deleteSubordinate("Alice", "David");
+//    empList.deleteEmployee("Charlie");
+//
+//    cout << "\nUpdated employee list:" << endl;
+//    empList.display();
+//
+//    return 0;
+//}
